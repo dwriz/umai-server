42aa165392b8c6de1e93ec0606fec870247141b3
dwriz <dwikyrizkya27@gmail.com>
2024-06-13 12:09:14 +0700

    refactor bcrypt.js

--- umai/helpers/bcrypt.js
+++ umai/helpers/bcrypt.js
@@ -2,10 +2,10 @@ const bcrypt = require("bcryptjs");
 
 function hashPassword(plainPassword) {
   return bcrypt.hashSync(plainPassword, bcrypt.genSaltSync(8));
-};
+}
 
 function comparePassword(plainPassword, hashedPassword) {
   return bcrypt.compareSync(plainPassword, hashedPassword);
-};
+}
 
 module.exports = { hashPassword, comparePassword };
